42c95fb9fc331a3aec40de4bedac7e9ffa7c0abf
matheuskdr <matheusgabriellkpo@gmail.com>
2025-08-16 11:30:54 -0300

    Routes and Controllers

---
+++ src/controllers/pageController.ts
@@ -0,0 +1,14 @@
+import { Request, Response } from "express";
+
+export const home = (req: Request, res: Response) => {
+    res.send('Welcome to the Home Page');
+}
+export const dogs = (req: Request, res: Response) => {
+
+}
+export const cats = (req: Request, res: Response) => {
+
+}
+export const fishes = (req: Request, res: Response) => {
+
+}
